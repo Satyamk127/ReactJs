f2072e5b02f59d996c511ad21648a692c9a068ed
Satyamkeshari <satyamkumarkeshari358@gmail.com>
2026-01-30 22:03:18 +0530

    componet

---
+++ 02-componet/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from './componet/Card'
+import Navbar from './componet/Navbar'
+// rafce
+const App = () => {
+  return (
+    <div>
+       <Navbar/>
+      <Card/>
+        <Card/>
+        <Card/>
+      
+      
+    </div>
+  )
+}
+
+export default App
---
+++ 02-componet/src/componet/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className='card'>
+        Lorem ipsum dolor sit amet. lore10
+        m ipsum dolor sit amet. lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.
+      </div>
+    </div>
+  )
+}
+
+export default Card
