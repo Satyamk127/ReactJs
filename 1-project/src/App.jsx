eb87c56b2ea67790fb8fcad3d30447f8d091fcf9
Satyamkeshari <satyamkumarkeshari358@gmail.com>
2026-01-30 21:29:53 +0530

    first

---
+++ 1-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+// rafce
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id="Child" > Hello My Raju</div>
+    <div id="child" >Child Hello</div>
+    </>
+      
+  )
+}
+
+export default App
