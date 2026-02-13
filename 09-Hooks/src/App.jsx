e630cf8373a5809e40486c4d306120239bc0dfd8
Satyamkeshari <satyamkumarkeshari358@gmail.com>
2026-02-13 14:17:42 +0530

    useState

---
+++ 09-Hooks/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// useStateSnippet
+function App() {
+       // read   write
+  const [first, setfirst] = useState(0)
+  function click(){
+    setfirst(first + 1)
+  }
+
+  return (
+    <>
+      <h1>This is the first Number: {first}</h1>
+      <button onClick={click}>change Number</button>
+        
+    </>
+  )
+}
+
+export default App
