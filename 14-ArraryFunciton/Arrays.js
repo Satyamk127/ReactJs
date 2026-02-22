20058f1c865a0057a61009c6fd150ba7f750e884
Satyamkeshari <satyamkumarkeshari358@gmail.com>
2026-02-22 10:58:22 +0530

    ArrayFuntion

---
+++ 14-ArraryFunciton/Arrays.js
@@ -0,0 +1,16 @@
+function add(a,b){
+  console.log(a+b);
+}
+add(10,20);
+
+var sub =(a,b)=>{
+  console.log(a-b);
+}
+add(10,20);
+sub(30,10);
+
+var hello=()=> console.log("Hello World");
+hello();
+ram =()=>"One line function without return";
+
+console.log(ram());
