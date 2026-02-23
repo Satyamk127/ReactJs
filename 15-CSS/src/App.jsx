fb27e619cd8797304065a3db22d04f5579b66d7a
Satyamkeshari <satyamkumarkeshari358@gmail.com>
2026-02-23 22:40:47 +0530

    good practice

---
+++ 15-CSS/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+ 
+  return (
+    <>
+      <div className='ram'>
+        <h3>Perfect Lighing  Desing</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam voluptate porro, saepe ipsum fugit veniam temporibus nam commodi enim earum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi temporibus fugit perspiciatis quas, veniam officia!</p>
+      <h2>More -&gt;</h2>
+      </div>
+       
+       
+       
+    </>
+  )
+}
+
+export default App
