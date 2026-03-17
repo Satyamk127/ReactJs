e9fcb688c1eb978bfcba578fdcad5742f0e7fe4e
Satyamkeshari <satyamkumarkeshari358@gmail.com>
2026-03-17 19:19:47 +0530

    useEffect

---
+++ 22-useEffect/src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const App=()=>{
+
+  const [count,setCount]= useState(0);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setCount(pros=> pros+1)
+    },2000)
+  },[2])
+  return (
+    <>
+    <h1>This is the {count} Increse by one</h1>
+
+    </>
+  );
+}
+export default App;
